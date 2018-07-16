5bf98c52bcb9fae6a3ee9b339bbd9c643bcd4892
Kevin Scott <kevin@hitheory.com>
2018-07-16 12:34:17 -0400

    Add ability to pass an HTMLImageElement

--- src/translateImages.ts
+++ src/translateImages.ts
@@ -1,5 +1,29 @@
-const translateImages = async (origImages: Array<tf.Tensor3D | HTMLImageElement>) => {
-  return origImages;
+const loadImage = async (src: string) => new Promise<HTMLImageElement>((resolve, reject) => {
+  const image = new Image();
+  image.src = src;
+  image.onload = () => resolve(image);
+  image.onerror = (err) => reject(err);
+});
+
+const translateImages = async (origImages: Array<tf.Tensor3D | HTMLImageElement | string>) => {
+  const images = [];
+
+  for (let i = 0; i < origImages.length; i++) {
+    let image = origImages[i];
+
+    if (typeof image === 'string') {
+      const loadedImage = await loadImage(image);
+      image = tf.fromPixels(loadedImage);
+    } else if (image.src) {
+      const loadedImage = await loadImage(image.src);
+      image = tf.fromPixels(loadedImage);
+    }
+    // else, it is a tensor
+
+    images.push(image);
+  }
+
+  return images;
 }
 
 export translateImages;
